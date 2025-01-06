b15af998a9f40a3bfbb1deb89e008240f6766cca
Sarthak Verma <sarthakvdev@gmail.com>
2025-01-06 20:11:50 +0530

    llm state and graph execution

--- index.py
+++ index.py
@@ -1,6 +1,12 @@
 from dotenv import load_dotenv
 import getpass
 import os
+from typing import Annotated
+from typing_extensions import TypedDict
+# langgraph imports
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
@@ -11,4 +17,47 @@ def _set_env(var: str):
   else :
     print(f"{var} already set")
 
+# State of the graph using TypedDict
+class State(TypedDict):
+  # Messages have the type "list".
+  messages: Annotated[list, add_messages]
+
+def chatbot(state: State):
+  return { "messages": llm.invoke(state["messages"]) }
+
+def stream_graph_updates(user_input: str):
+  for event in graph.stream({"messages": [("user", user_input)]}):
+    for value in event.values():
+      print("Assistant", value["messages"][-1].content)
+
+llm = ChatOpenAI(
+    model="gpt-4o",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+
+# --- Executions --- #
 _set_env("OPENAI_API_KEY")
+
+graph_builder = StateGraph(State)
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+graph = graph_builder.compile()
+
+while True:
+  try:
+    user_input = input("User:")
+    if user_input.lower() in ["quit", "q", "exit"]:
+      print("Goodbye!")
+      break
+    
+    stream_graph_updates(user_input)
+  except:
+    # fallback if input() in not available
+    user_input = "What do you know about LangGraph?"
+    print("User: " + user_input)
+    stream_graph_updates(user_input)
+    break
